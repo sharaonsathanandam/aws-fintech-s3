63efe0698e82830eb5f3d2065d8bcbadd779ee29
sharaonabishek <sharaonm@bbinsight.com>
2025-05-12 19:17:35 -0500

    Add new bucket files

--- pipeline-config/backend.tf
+++ pipeline-config/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "fintech-tfstate-bucket"
+    bucket         = "fintech-s3-tfstate-bucket"
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "s3-terraform-locks"
   }
 }
